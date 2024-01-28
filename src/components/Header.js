bacde325a29de0c6bc83fbaf6fbdd25f5a5e6807
Shubham Sarda <shubhamsarda99@yahoo.com>
2024-01-28 16:25:39 +0530

    Update Header.js (theme names)

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,9 @@ const Header = ({children, theme, setTheme}) => {
                 <span className={theme === "light" ? "light activeTheme" : "light"} onClick={() => setTheme("light")}></span>
                 <span className={theme === "medium" ? "medium activeTheme" : "medium"} onClick={() => setTheme("medium")}></span>
                 <span className={theme === "dark" ? "dark activeTheme" : "dark"} onClick={() => setTheme("dark")}></span>
-                <span className={theme === "gradientOne" ? "gradientOne activeTheme" : "gradientOne"} onClick={() => setTheme("gradientOne")}></span>
-                <span className={theme === "gradientTwo" ? "gradientTwo activeTheme" : "gradientTwo"} onClick={() => setTheme("gradientTwo")}></span>
-                <span className={theme === "gradientThree" ? "gradientThree activeTheme" : "gradientThree"} onClick={() => setTheme("gradientThree")}></span>
+                <span className={theme === "gradientOne" ? "gtOne activeTheme" : "gradientOne"} onClick={() => setTheme("gradientOne")}></span>
+                <span className={theme === "gradientTwo" ? "gTwo activeTheme" : "gradientTwo"} onClick={() => setTheme("gradientTwo")}></span>
+                <span className={theme === "gradientThree" ? "gThree activeTheme" : "gradientThree"} onClick={() => setTheme("gradientThree")}></span>
             </span>
         </header>
     )
